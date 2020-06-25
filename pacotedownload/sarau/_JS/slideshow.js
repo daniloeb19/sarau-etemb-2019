1f52d2efa598ce2480c8dd011c03945ffc4f906c
daniloeb19 <daniloferreiradasilva345@gmail.com>
2020-06-24 23:09:42 -0300

    commit all files

---
+++ pacotedownload/sarau/_JS/slideshow.js
@@ -0,0 +1,161 @@
+/**
+ * @file slideshow.js
+ * @author William Bruno
+ * @date 2013-05-25
+ */
+ var adcast = (function (window, document) {
+    "use strict";
+    /*jslint plusplus: true, browser: true */
+    var module = {
+        timer : 0,
+        atual : 0,
+        max   : 1,
+        delay : 3600000,
+        config : function (config) {
+            module.$adcastWrap  = config.adcasts[0].parentNode;
+            module.$adcasts     = [].slice.call(config.adcasts);
+            module.$pagers      = [].slice.call(config.pagers);
+            module.onChange     = config.onChange;
+            module.$prev        = config.prev;
+            module.$next        = config.next;
+            module.pagersClick  = config.pagersClick || false;
+
+            module.max = module.$adcasts.length;
+        },
+        infinite : function (i) {
+            var prev = i > 0 ? i - 1 : module.max - 1,
+            next = i < module.max - 1 ? i + 1 : 0;
+
+            module.$adcasts[prev].classList.add('prev');
+            module.$adcasts[next].classList.add('next');
+        },
+        active : function () {
+            var i = module.atual;
+            module.$adcasts[i].classList.add('is-active');
+            module.$pagers[i].classList.add('is-active');
+
+
+            module.$adcastWrap.className = 'adcast-item-' + i;
+
+
+            if (module.onChange) {
+                module.onChange(i);
+            }
+        },
+        none : function ($els) {
+            $els.forEach(function ($el) {
+                $el.classList.remove('is-active','prev','next');
+            });
+        },
+        next : function () {
+            var i = module.atual;
+            module.none(module.$adcasts);
+            module.none(module.$pagers);
+            module.active();
+            module.infinite(i);
+            
+        },
+        _setAtual : function (i) {
+
+            if (i < 0) {
+                module.atual = module.max - 1;
+
+            } else {
+                module.atual = i < module.max ? parseInt(i, 10) : 0;
+            }
+        },
+        _auto : function () {
+            module.timer = window.setInterval(function () {
+                module._setAtual(module.atual + 1);
+                module.next();
+            }, module.delay);
+        },
+        _pause: function () {
+            window.clearInterval(module.timer);
+            window.clearTimeout(module.tot);
+        },
+        _onPagersClick : function ($pager) {
+            $pager.addEventListener('click', function () {
+                var $this = this,
+                i = parseInt($this.getAttribute('data-adcast'), 10);
+                module._pause();
+                module.tot = window.setTimeout(module._auto, module.delay / 2);
+
+                module._setAtual(i);
+                module.next();
+            });
+        },
+        _onPagersHover : function ($pager) {
+            $pager.addEventListener('mouseover', function () {
+                var $this = this,
+                i = parseInt($this.getAttribute('data-adcast'), 10);
+
+                module.atual = i;
+                window.clearInterval(module.timer);
+                module.next();
+            });
+            $pager.addEventListener('mouseout', function () {
+                var $this = this,
+                i = parseInt($this.getAttribute('data-adcast'), 10);
+
+                module._setAtual(i);
+                module._auto();
+            });
+        },
+        _keyboard : function () {
+            document.addEventListener('keydown', function (event){
+                var i;
+                if (event.keyCode === 39 || event.keyCode === 76) { //right
+                    i = module.atual + 1;
+                }
+                if (event.keyCode === 37 || event.keyCode === 72) { //left
+                    i = module.atual - 1;
+                }
+
+                module._pause();
+                module.tot = window.setTimeout(module._auto, module.delay / 2);
+                module._setAtual(i);
+                module.next();
+            });
+        },
+        _arrows : function () {
+            if (module.$next) {
+              module.$next.addEventListener('click', function(){
+                  module._setAtual(module.atual + 1);
+                  module.next();
+              });
+          }
+          if (module.$prev) {
+              module.$prev.addEventListener('click', function(){
+                  module._setAtual(module.atual - 1);
+                  module.next();
+              });
+          }
+      },
+      events : function () {
+        module.$pagers.forEach(function ($pager) {
+            if (module.pagersClick) {
+                module._onPagersClick($pager);
+            } else {
+                module._onPagersHover($pager);
+            }
+        });
+        module._keyboard();
+        module._arrows();
+    },
+    init : function (config) {
+        module.config(config);
+
+        if (!module.max || module.max === 1) return;
+
+        module.next();
+        if (config.auto) module._auto();
+        module.events();
+    }
+};
+
+return {
+    init : module.init
+};
+
+}(window, document));
---
+++ pacotedownload/sarau/_JS/util.js
@@ -0,0 +1,339 @@
+function apagar() {
+	try{
+		document.getElementById("b1").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b2").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b3").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b4").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b5").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b6").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b7").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b8").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b9").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b10").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b11").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b12").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b13").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b14").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b15").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b16").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b17").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b18").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b19").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b20").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b21").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b22").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b23").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b24").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b25").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b26").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b27").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b28").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b29").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b30").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b31").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b32").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b33").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b34").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b35").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b36").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b37").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b38").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b39").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b40").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b41").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b42").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b43").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b44").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b45").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b46").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b47").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b48").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b49").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b50").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b51").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b52").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b53").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b54").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b55").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b56").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b57").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b58").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b59").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b60").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	
+	try{
+		document.getElementById("b62").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b63").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b64").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b65").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b66").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b67").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b68").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b69").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b70").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b71").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b72").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b73").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b74").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b75").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b76").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b77").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b78").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b79").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b80").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b81").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b82").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b83").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b84").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+	try{
+		document.getElementById("b85").style.backgroundColor = "#2196F3";"."
+	}catch(e){
+	}
+}
